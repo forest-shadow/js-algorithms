0cf9c85ed8a551c0c2e580916b87c24c0e33be71
forest-shadow <mamasay@yandex.ru>
2020-02-16 10:59:53 +0300

    move recursion tests to separate folder

--- 04_recursion/sumRange.test.js
+++
@@ -1,8 +0,0 @@
-import sumRange from './sumRange'
-
-describe('sumRange', () => {
-  it('should return sum from 1 to `num`', () => {
-    expect(sumRange(5)).toEqual(15)
-    expect(sumRange(4)).toEqual(10)
-  });
-});
---
+++ 04_recursion/tests/factorial.test.js
@@ -0,0 +1,8 @@
+import factorial from '../factorial';
+
+describe('factorial', () => {
+  it('should return `n` factorial', () => {
+    expect(factorial(3)).toEqual(6);
+    expect(factorial(4)).toEqual(24);
+  });
+});
