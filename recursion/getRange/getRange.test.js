4d455dd3967a124b247cea80139b0b0af217ef62
forest-shadow <ivan.arevkov@gmail.com>
2023-01-17 22:08:23 +0300

    refactor(getRange)
    * added readme file
    * fixed implementation
    * added respective test case

--- recursion/getRange/getRange.test.js
+++ recursion/getRange/getRange.test.js
@@ -1,7 +1,8 @@
-import getRange from '../getRange';
+import getRange from './getRange';
 
 describe('getRange', () => {
   it('should return array of integers from `start` to `end`', () => {
+    expect(getRange(1, 6)).toEqual([1, 2, 3, 4, 5, 6]);
     expect(getRange(2, 7)).toEqual([2, 3, 4, 5, 6, 7]);
   });
 });
---
+++ recursion/getRange/readme.md
@@ -0,0 +1,7 @@
+# getRange
+
+---
+Write recursive `getRange` function to get the integers in range (x, y)
+```js
+getRange(1, 6); // [1, 2, 3, 4, 5, 6]
+```
