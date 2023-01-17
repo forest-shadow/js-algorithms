d1dfb0e326ebd9169a4a51d0f74da9d08e8a57db
forest-shadow <ivan.arevkov@gmail.com>
2023-01-17 18:52:35 +0300

    refactor(factorial)
    * added readme file
    * fixed implementation
    * added respective test case

---
+++ recursion/factorial/readme.md
@@ -0,0 +1,10 @@
+# factorial
+
+---
+
+Write `factorial` function computing the factorial of provided inter number.
+```js
+factorial(3); // 6
+factorial(4); // 24
+factorial(5); // 120
+```
--- recursion/tests/factorial.test.js
+++
@@ -1,8 +0,0 @@
-import factorial from '../factorial';
-
-describe('factorial', () => {
-  it('should return `n` factorial', () => {
-    expect(factorial(3)).toEqual(6);
-    expect(factorial(4)).toEqual(24);
-  });
-});
