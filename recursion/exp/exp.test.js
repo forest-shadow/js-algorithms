e035c241a771163de507327a530cc26e7020d025
forest-shadow <ivan.arevkov@gmail.com>
2023-01-17 17:52:47 +0300

    refactor(exp)
    * added readme file
    * fixed implementation
    * added respective test case

--- recursion/exp/exp.test.js
+++ recursion/exp/exp.test.js
@@ -1,7 +1,9 @@
-import exp from '../exp';
+import exp from './exp';
 
 describe('exp', () => {
   it('should return powered value', () => {
+    expect(exp(2, 3)).toEqual(8);
     expect(exp(5, 3)).toEqual(125);
+    expect(exp(5, 5)).toEqual(3125);
   });
 });
---
+++ recursion/exp/readme.md
@@ -0,0 +1,11 @@
+# exp
+
+---
+
+Write `exp` recursive function that raises given number to specified power
+
+```js
+exp(2, 3); // 8
+exp(5, 3); // 125
+exp(5, 5); // 3125
+```
