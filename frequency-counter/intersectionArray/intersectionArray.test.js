5c959003434282670143bda2ab61c56a5b5d0659
forest-shadow <ivan.arevkov@gmail.com>
2023-03-24 18:12:20 +0100

    feat(intersectionArray)
    * added related test cases and description

---
+++ frequency-counter/intersectionArray/intersectionArray.test.js
@@ -0,0 +1,12 @@
+import {intersectionArray} from './intersectionArray';
+
+describe('intersectionArray', () => {
+  it('should return null if empty arrays will be provided', () => {
+    expect(intersectionArray([], [])).toEqual(null);
+  })
+  it('should return array of intersection elements', () => {
+    const arr1 = [2, 5, 7, 6, 2, 4, 4];
+    const arr2 = [3, 4, 6, 7, 2, 1, 2];
+    expect(intersectionArray(arr1, arr2)).toEqual([2, 4, 6, 7]);
+  });
+});
