369bed4171f989142f2854f3d1a801d70531e1a7
forest-shadow <ivan.arevkov@gmail.com>
2023-03-24 18:00:23 +0100

    fix(linearSearch) fixed linearSearch test case
    * updated `.babelrc` config

--- searching/linearSearch.test.js
+++ searching/linearSearch.test.js
@@ -1,8 +1,8 @@
 import linearSearch from './linearSearch'
 
-const arr = ['5', '2', '10', '15']
+const arr = [5, 2, 10, 15]
 
-describe('linearSearch', function () {
+describe('linearSearch', () => {
   it('should return searched element index inside of array', () => {
     expect(linearSearch(arr, 10)).toEqual(2)
     expect(linearSearch(arr, 15)).toEqual(3)
