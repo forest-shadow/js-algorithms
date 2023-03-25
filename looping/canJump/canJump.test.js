32f874dfe3ee90399475eaaf98c64e984fe5cccb
forest-shadow <ivan.arevkov@gmail.com>
2023-03-25 14:32:53 +0100

    feat(canJump)
    * added related test cases and description

---
+++ looping/canJump/canJump.test.js
@@ -0,0 +1,16 @@
+import {canJump} from "./canJump";
+
+describe('canJump', () => {
+  it('should return false if null or empty array provided', () => {
+    expect(canJump(null)).toBeFalsy();
+    expect(canJump([])).toBeFalsy();
+  });
+  it('should return if last array element is reachable', () => {
+    expect(canJump([2, 0, 0])).toBeTruthy();
+    expect(canJump([2, 3, 1, 1, 4])).toBeTruthy();
+    expect(canJump([2, 3, 0, 0, 4])).toBeTruthy();
+  });
+  it('should return false if last array element is unreachable', () => {
+    expect(canJump([3, 2, 1, 0, 4])).toBeFalsy();
+  });
+});
