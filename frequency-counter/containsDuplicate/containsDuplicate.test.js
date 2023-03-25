d61313d923494bbd2c12c74e310d729b42d01418
forest-shadow <ivan.arevkov@gmail.com>
2023-03-25 15:00:19 +0100

    feat(containsDuplicate)
    * added related test cases and description

---
+++ frequency-counter/containsDuplicate/containsDuplicate.test.js
@@ -0,0 +1,16 @@
+import {containsDuplicate} from "./containsDuplicate";
+
+describe('containsDuplicate', () => {
+  it('should return false if empty or no array was provided', () => {
+    expect(containsDuplicate()).toBeFalsy();
+    expect(containsDuplicate([])).toBeFalsy();
+  });
+  it('should return false in provided array contains only unique elements', () => {
+    expect(containsDuplicate([1,2,3,4,5])).toBeFalsy();
+    expect(containsDuplicate([2,6,8,9,1])).toBeFalsy();
+  });
+  it('should return true if provided array contains duplicated elements', () => {
+    expect(containsDuplicate([1,2,3,4,1,2,3])).toBeTruthy();
+    expect(containsDuplicate([5,1,2,3,4,5])).toBeTruthy();
+  });
+});
---
+++ frequency-counter/containsDuplicate/readme.md
@@ -0,0 +1,14 @@
+# Check if array contains duplicated numbers
+
+### Description
+Write function checking if current numeric array contains duplicates.
+
+### Implementation details
+
+```js
+containsDuplicate();                 // false
+containsDuplicate([]);               // false
+containsDuplicate([1,2,3,4,5]);      // false
+containsDuplicate([1,2,3,4,1,2,3]);  // true
+containsDuplicate([5,1,2,3,4,5]);    // true
+```
