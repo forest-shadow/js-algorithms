d335c8ec6b15e8a50a189f518d4e398663c6014e
forest-shadow <ivan.arevkov@gmail.com>
2023-04-06 14:02:33 +0200

    refactor(maxSubarraySum)
    * moved algorithm to separate directory

--- sliding-window/maxSubarraySum/maxSubarraySum.js
+++ sliding-window/maxSubarraySum/maxSubarraySum.js
@@ -1,12 +1,3 @@
-// Max consecutive sum of `n` element of array
-// Write a function called `maxSubarraySum` which accepts an array of integers and a number called n. The function should calculate the maximum sum of n consecutive elements in array.
-
-// maxSubarraySum([1,2,5,2,8,1,5],2)   // 10
-// maxSubarraySum([1,2,5,2,8,1,5],4)   // 17
-// maxSubarraySum([4,2,1,6],1)         // 6
-// maxSubarraySum([4,2,1,6,2],4)       // 13
-// maxSubarraySum([],4)                // null
-
 // naive solution here based on squared loop
 // Time Complexity - O(N^2)
 function maxSubarraySumNaive (arr, num) {
