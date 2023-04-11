5375e04523a9e69a9ae2dd8be1dde7aa495c91e1
forest-shadow <ivan.arevkov@gmail.com>
2023-04-11 11:43:34 +0200

    refactor(intersectionArray)
    * refactored to more concise solution

--- frequency-counter/intersectionArray/intersectionArray.js
+++ frequency-counter/intersectionArray/intersectionArray.js
@@ -1,19 +1,13 @@
 export const intersectionArray = (arr1, arr2) => {
-  if(!arr1.length && !arr2.length) return null;
-  const result = [];
-  const arr1Set = new Set(arr1.sort());
-  const arr2Set = new Set(arr2.sort());
-
-  const arr1Counter = {};
-  for(let el of arr1Set) {
-    if(arr1Counter[el]) {
-      arr1Counter[el]++;
-    } else {
-      arr1Counter[el] = 1;
-    }
+  if(!arr1.length || !arr2.length) return null;
+  
+  const state = {};
+  const result = new Set();
+  for(let num of arr1) {
+    if(!state[num]) state[num] = true;
   }
-  for(let el of arr2Set) {
-    if(arr1Counter[el]) result.push(el);
+  for(let num of arr2) {
+    if(state[num]) result.add(num)
   }
-  return result.sort();
-};
+  return Array.from(result).sort();
+}
