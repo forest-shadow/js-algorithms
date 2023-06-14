f7906e8ef855fdbff5ab7b4d835389627ff2f224
forest-shadow <ivan.arevkov@gmail.com>
2023-06-14 16:53:58 +0200

    chore(containsDuplicate) deleted redundant logs
    * fixed formatting

--- frequency-counter/containsDuplicate/containsDuplicate.js
+++ frequency-counter/containsDuplicate/containsDuplicate.js
@@ -1,15 +1,9 @@
-export const containsDuplicate = arr => {
-  if(!arr || !arr.length) return false;
+export const containsDuplicate = (arr) => {
+  if (!arr || !arr.length) return false;
   const arrHash = {};
-  for(let el of arr) {
-    if(arrHash[el]) return true;
-      else arrHash[el] = true;
+  for (let el of arr) {
+    if (arrHash[el]) return true;
+    else arrHash[el] = true;
   }
   return false;
 };
-
-console.log(containsDuplicate()) // false
-console.log(containsDuplicate([])) // false
-console.log(containsDuplicate([1,2,3,4,5])) // false
-console.log(containsDuplicate([1,2,3,4,1,2,3])) // true
-console.log(containsDuplicate([5, 1,2,3,4,5])) // true
--- frequency-counter/containsDuplicate/containsDuplicate.test.js
+++ frequency-counter/containsDuplicate/containsDuplicate.test.js
@@ -1,4 +1,4 @@
-import {containsDuplicate} from "./containsDuplicate";
+import { containsDuplicate } from './containsDuplicate';
 
 describe('containsDuplicate', () => {
   it('should return false if empty or no array was provided', () => {
@@ -6,11 +6,11 @@ describe('containsDuplicate', () => {
     expect(containsDuplicate([])).toBeFalsy();
   });
   it('should return false in provided array contains only unique elements', () => {
-    expect(containsDuplicate([1,2,3,4,5])).toBeFalsy();
-    expect(containsDuplicate([2,6,8,9,1])).toBeFalsy();
+    expect(containsDuplicate([1, 2, 3, 4, 5])).toBeFalsy();
+    expect(containsDuplicate([2, 6, 8, 9, 1])).toBeFalsy();
   });
   it('should return true if provided array contains duplicated elements', () => {
-    expect(containsDuplicate([1,2,3,4,1,2,3])).toBeTruthy();
-    expect(containsDuplicate([5,1,2,3,4,5])).toBeTruthy();
+    expect(containsDuplicate([1, 2, 3, 4, 1, 2, 3])).toBeTruthy();
+    expect(containsDuplicate([5, 1, 2, 3, 4, 5])).toBeTruthy();
   });
 });
