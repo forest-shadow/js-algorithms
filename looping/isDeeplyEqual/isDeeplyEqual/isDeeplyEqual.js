40309775c68c78fd3c9fd20c4abb7dfb5259ea4c
forest-shadow <ivan.arevkov@gmail.com>
2023-07-31 13:18:53 +0200

    fix(looping/isDeeplyEqual)

--- looping/isDeeplyEqual/isDeeplyEqual/isDeeplyEqual.js
+++ looping/isDeeplyEqual/isDeeplyEqual/isDeeplyEqual.js
@@ -11,14 +11,13 @@ export const isDeeplyEqual = (val1, val2) => {
     if (typeof val1 !== typeof val2) return false;
     if ((val1 === null || val2 === null) && val1 !== val2) return false;
     if (typeof val1 === 'object') {
-      for(let key in val1) {
-        stack.push({val1: val1[key], val2: val2[key]})
+      for (let key in val1) {
+        stack.push({ val1: val1[key], val2: val2[key] });
       }
-      console.log('stack', stack);
     } else {
-      if(val1 !== val2) return false;
+      if (val1 !== val2) return false;
     }
   }
 
   return true;
-}
+};
