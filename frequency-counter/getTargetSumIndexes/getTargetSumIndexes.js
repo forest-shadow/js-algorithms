dbb9af001daa19f861743bc73821e973dc9977ae
forest-shadow <ivan.arevkov@gmail.com>
2024-02-14 15:11:41 +0100

    feat(getTargetSumIndexes) updated implementation
    * added edge case when lookup value (index) is equal to zero

--- frequency-counter/getTargetSumIndexes/getTargetSumIndexes.js
+++ frequency-counter/getTargetSumIndexes/getTargetSumIndexes.js
@@ -3,7 +3,7 @@ export const getTargetSumIndexes = (nums, targetSum) => {
   for(let i = 0; i <= nums.length; i++) {
     const current = nums[i];
     const currentDiff = targetSum - current;
-    if(state[current]) return [state[current], i]
+    if(state[current] >= 0) return [state[current], i]
       else state[currentDiff] = i;
   }
 }
--- frequency-counter/getTargetSumIndexes/getTargetSumIndexes.test.js
+++ frequency-counter/getTargetSumIndexes/getTargetSumIndexes.test.js
@@ -4,5 +4,7 @@ describe('getTargetSumIndexes', () => {
   it('should return indexes of integer array elements which add up to target sum value', () => {
     expect(getTargetSumIndexes([2, 1, 2, 6, 4], 7)).toEqual([1, 3]);
     expect(getTargetSumIndexes([2, 1, 2, 6, 4], 5)).toEqual([1, 4]);
+    expect(getTargetSumIndexes([3, 2, 4], 6)).toEqual([1, 2]);
+    expect(getTargetSumIndexes([3, 3], 6)).toEqual([0, 1]);
   })
 })
