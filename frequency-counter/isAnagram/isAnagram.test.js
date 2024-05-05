9884ed4e0ab8354d0ea721eaa965819143ef7688
forest-shadow <ivan.arevkov@gmail.com>
2024-05-05 10:53:05 +0200

    fix(isAnagram) updated text description

--- frequency-counter/isAnagram/isAnagram.test.js
+++ frequency-counter/isAnagram/isAnagram.test.js
@@ -6,7 +6,7 @@ describe('isAnagram', () => {
     expect(isAnagram('qwerty', 'weyqrt')).toBe(true);
     expect(isAnagram('anagram', 'nagaram')).toBe(true);
   });
-  test('should return false if second string parameter is not isAnagram of first one', () => {
+  test('should return false if second string parameter is not an anagram of the first one', () => {
     expect(isAnagram('aaz', 'zza')).toBe(false);
     expect(isAnagram('rat', 'car')).toBe(false);
     expect(isAnagram('awesome', 'awesom')).toBe(false);
