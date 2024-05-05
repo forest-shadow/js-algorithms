72e0cac1cff258e5fc444fb3a7654737a4f7baea
forest-shadow <ivan.arevkov@gmail.com>
2024-05-05 09:57:33 +0200

    feat(getTargetSumIndexes) updated implementation

--- frequency-counter/getTargetSumIndexes/getTargetSumIndexes.js
+++ frequency-counter/getTargetSumIndexes/getTargetSumIndexes.js
@@ -1,9 +1,12 @@
-export const getTargetSumIndexes = (nums, targetSum) => {
-  const state = {};
-  for(let i = 0; i <= nums.length; i++) {
-    const current = nums[i];
-    const currentDiff = targetSum - current;
-    if(state[current] >= 0) return [state[current], i]
-      else state[currentDiff] = i;
+export const getTargetSumIndexes = (numbers, sum) => {
+  const numbersDiffHash = {};
+  for(let i = 0; i < numbers.length; i++) {
+    const currentNum = numbers[i];
+    const currentNumDiff = sum - currentNum;
+    if(numbersDiffHash[currentNum] >= 0) {
+      return [numbersDiffHash[currentNum], i]
+    } else {
+      numbersDiffHash[currentNumDiff] = i;
+    }
   }
 }
