e463c4725afb110b6a5a0c26a4bc5c5d9d1b675c
forest-shadow <ivan.arevkov@gmail.com>
2024-05-13 06:45:17 +0200

    fix(intersectionArray) updated implementation
    * added jsdoc
    * updated code
    * updated test case

--- frequency-counter/intersectionArray/intersectionArray.js
+++ frequency-counter/intersectionArray/intersectionArray.js
@@ -1,13 +1,18 @@
-export const intersectionArray = (arr1, arr2) => {
-  if(!arr1.length || !arr2.length) return null;
-  
-  const state = {};
-  const result = new Set();
-  for(let num of arr1) {
-    if(!state[num]) state[num] = true;
+/**
+ * intersectionArray - return array of intersected elements
+ * @param {number[]} numbers1 
+ * @param {number[]} numbers2 
+ * @returns {number[]|null}
+ */
+export const intersectionArray = (numbers1, numbers2) => {
+  if (!numbers1.length || !numbers2.length) return null;
+  const numbers1Map = {};
+  const intersections = new Set();
+  for (let number of numbers1) {
+    numbers1Map[number] = true;
   }
-  for(let num of arr2) {
-    if(state[num]) result.add(num)
+  for (let number of numbers2) {
+    if (numbers1Map[number]) intersections.add(number);
   }
-  return Array.from(result).sort();
+  return Array.from(intersections).sort();
 }
--- frequency-counter/intersectionArray/intersectionArray.test.js
+++ frequency-counter/intersectionArray/intersectionArray.test.js
@@ -1,8 +1,10 @@
 import {intersectionArray} from './intersectionArray';
 
 describe('intersectionArray', () => {
-  it('should return null if empty arrays will be provided', () => {
+  it('should return null if at leash one empty number array will be provided', () => {
     expect(intersectionArray([], [])).toEqual(null);
+    expect(intersectionArray([1,2], [])).toEqual(null);
+    expect(intersectionArray([], [1,2])).toEqual(null);
   })
   it('should return array of intersection elements', () => {
     const arr1 = [2, 5, 7, 6, 2, 4, 4];
