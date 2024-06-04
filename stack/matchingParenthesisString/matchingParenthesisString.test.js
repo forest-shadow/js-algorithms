9e251f95931e4f91d4f390a14aac71ddc40ed9a4
Ivan Arevkov <ivan.arevkov@gmail.com>
2024-06-04 13:55:52 +0300

    chore: updated test case

--- stack/matchingParenthesisString/matchingParenthesisString.test.js
+++ stack/matchingParenthesisString/matchingParenthesisString.test.js
@@ -2,12 +2,12 @@ import { isMatchingParenthesisString } from './matchingParenthesisString';
 
 describe('matchingParenthesisString', () => {
   it('should return true if provided parenthesis pattern is valid', () => {
-    isMatchingParenthesisString('()')     // true
-    isMatchingParenthesisString('()[]{}') // true
+    isMatchingParenthesisString('()');
+    isMatchingParenthesisString('()[]{}');
   });
 
   it('should return false if provided parenthesis pattern is invalid', () => {
-    isMatchingParenthesisString('([)]')   // false
-    isMatchingParenthesisString('(]')      // false
+    isMatchingParenthesisString('([)]');
+    isMatchingParenthesisString('(]');
   });
 })
