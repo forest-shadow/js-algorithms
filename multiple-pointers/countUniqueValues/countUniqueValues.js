5fe4b9d5df39604815b45a4bb828b98082c839e7
Ivan Arevkov <ivan.arevkov@gmail.com>
2024-08-15 10:38:00 +0300

    fix(countUniqueValues) updated solution

--- multiple-pointers/countUniqueValues/countUniqueValues.js
+++ multiple-pointers/countUniqueValues/countUniqueValues.js
@@ -4,23 +4,36 @@
  * @returns {number}
  */
 const countUniqueValues = nums => {
-  if (nums.length === 0) return 0;
-  let low = 0;
-  let high = low + 1;
+  if(!nums.length) return 0;
+  let result = 1;
 
-  while(high < nums.length) {
-    if(nums[low] !== nums[high]) {
-      low++;
-      nums[low] = nums[high];
-
-      // or
-      // nums[low + 1] = nums[high];
-      // low++;
-    }
-    high++;
+  for(let i = 1; i < nums.length; i++) {
+    if (nums[i - 1] !== nums[i]) result++;
   }
 
-  return low + 1
+  return result;
 }
+
+// or
+// const countUniqueValues = nums => {
+//   if (nums.length === 0) return 0;
+//   let low = 0;
+//   let high = low + 1;
+
+//   while(high < nums.length) {
+//     if(nums[low] !== nums[high]) {
+//       low++;
+//       nums[low] = nums[high];
+
+//       // or
+//       // nums[low + 1] = nums[high];
+//       // low++;
+//     }
+//     high++;
+//   }
+
+//   return low + 1
+// }
+
 
 export default countUniqueValues;
