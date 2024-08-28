5661d44b9ed7da57f994c127315700b1c26d2f95
Ivan Arevkov <ivan.arevkov@gmail.com>
2024-08-28 21:41:48 +0300

    fix(flattenArr) updated exports & implementation

--- recursion/flattenArr/flattenArr.js
+++ recursion/flattenArr/flattenArr.js
@@ -1,14 +1,25 @@
 /**
  * `flattenArr` - function for flattening provided array
  *  [1, [1, 2, [3, 4]], [2, 4]] -> [1, 1, 2, 3, 4, 2, 4]
- *  @param {*} arr
+ *  @param {number[]} nums
  *  @return {number[]}  
  */
-export const flattenArr = arr => arr.reduce((result, element) => {
-  if(Array.isArray(element)) {
-    result = [...result, ...flattenArr(element)];
-  } else {
-    result = [...result, element]
+const flattenArr = nums => {
+  let result = [];
+  for(let el of nums) {
+    if (Array.isArray(el)) result = result.concat(flattenArr(el));
+      else result.push(el);
   }
-  return result;
-}, []);
+  return result
+}
+
+// const flattenArr = nums => arr.reduce((result, el) => {
+//   if(Array.isArray(el)) {
+//     result = [...result, ...flattenArr(el)];
+//   } else {
+//     result = [...result, el]
+//   }
+//   return result;
+// }, []);
+
+export default flattenArr;
