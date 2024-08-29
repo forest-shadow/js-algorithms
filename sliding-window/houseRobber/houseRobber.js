329f3cf9a6e12695fd285092c08e825647c9b54f
Ivan Arevkov <ivan.arevkov@gmail.com>
2024-08-29 10:42:40 +0300

    fix(houseRobber) updated export, added jsdoc

--- sliding-window/houseRobber/houseRobber.js
+++ sliding-window/houseRobber/houseRobber.js
@@ -1,18 +1,21 @@
-export const rob = nums => {
-  if(!nums || nums.length === 0) {
-    return 0;
+/**
+ * Return max sum of not adjacent elements
+ * @param {number[]} nums 
+ * @returns {number}
+ */
+const rob = nums => {
+  if (!nums || !nums.length) return 0;
+  if (nums.length === 1) return nums[0];
+  let result = [];
+
+  result[0] = nums[0];
+  result[1] = Math.max(nums[0], nums[1]);
+  
+  for (let i = 2; i < nums.length; i++) {
+    result[i] = Math.max(result[i - 1], result[i - 2] + nums[i]);
   }
 
-  if(nums.length === 1) {
-    return nums[0];
-  }
-
-  let total = [];
-  total[0] = nums[0];
-  total[1] = Math.max(nums[0], nums[1]);
-
-  for(let i = 2; i < nums.length; i++) {
-    total[i] = Math.max(total[i - 1], (total[i - 2] + nums[i]));
-  }
-  return total[total.length - 1]
+  return result[result.length - 1];
 }
+
+export default rob;
--- sliding-window/houseRobber/houseRobber.test.js
+++ sliding-window/houseRobber/houseRobber.test.js
@@ -1,4 +1,4 @@
-import { rob } from './houseRobber';
+import rob from './houseRobber';
 
 describe('rob', () => {
   it('should return maximum amount of gold that could be robbed without worrying the police', () => {
@@ -6,6 +6,7 @@ describe('rob', () => {
     expect(rob([1, 8, 3, 7, 9, 10])).toEqual(25);
     expect(rob([1, 2, 3, 1])).toEqual(4);
     expect(rob([2, 7, 9, 3, 1])).toEqual(12);
+    expect(rob([9, 1, 1, 1, 10])).toEqual(20);
   });
   it('should return zero if no input or empty array was', () => {
     expect(rob()).toEqual(0);
