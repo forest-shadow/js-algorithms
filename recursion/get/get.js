0070d696d63e2a017e0a9a9010bd0bd6652f5ff3
Ivan Arevkov <ivan.arevkov@gmail.com>
2024-09-20 10:43:47 +0300

    fix(get)
    * updated jsdoc
    * updated test case

--- recursion/get/get.js
+++ recursion/get/get.js
@@ -1,18 +1,16 @@
 /**
- * get - retunr object's property by provided path
+ * get - return object's property by provided path
  * @param {Object.<string, any>} obj 
  * @param {string} path 
  */
-export const get = (obj, path) => {
-  const [firstKey, ...lastKey] = path.split('.');
-
-  if(firstKey) {
-    if (typeof obj[firstKey] === 'object') {
-      return get(obj[firstKey], lastKey.join('.'))
-    } else if (obj[firstKey]) {
-      return obj[firstKey];
-    } else {
-      return null;
-    }
+const get = (obj, path) => {
+  const [firstKey, ...lastKeys] = path.split('.');
+  if(typeof obj[firstKey] === 'object') {
+    return get(obj[firstKey], lastKeys.join('.'))
+  } else if (obj[firstKey]) {
+    return obj[firstKey];
   }
+  return null;
 }
+
+export default get;
--- recursion/get/get.test.js
+++ recursion/get/get.test.js
@@ -1,4 +1,4 @@
-import { get } from './get';
+import get from './get';
 
 const obj = {
   a: {
